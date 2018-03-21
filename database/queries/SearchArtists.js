a6c60d826bf217f065127a4c17826fb9f4996289
Jordany Rosas <jordanyryan@gmail.com>
2018-03-21 17:28:03 -0400

    Added pagination functionality to show updated count of records on a given search criteria

--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -15,11 +15,12 @@ module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
   .skip(offset)
   .limit(limit)
   
-  return Promise.all([query, Artist.count()])
+  return Promise.all([query, Artist
+  .find(buildQuery(criteria)).count()])
     .then(results => {
       return {
         all: results[0],
-        count: results[0].length,
+        count: results[1],
         offset,
         limit
       }
